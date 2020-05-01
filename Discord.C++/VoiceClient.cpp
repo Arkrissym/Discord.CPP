fbd2966c9aacf4879e6a7f3a6fad24d0fae741d6
Arkrissym <Arkrissym@users.noreply.github.com>
2020-05-02 00:40:52 +0200

    use new method for ip discovery

--- Discord.C++/VoiceClient.cpp
+++ Discord.C++/VoiceClient.cpp
@@ -15,11 +15,11 @@
 #include <sodium.h>
 #include <opus/opus.h>
 
-#define FRAME_MILLIS 20
-#define FRAME_SIZE 960
-#define SAMPLE_RATE 48000
-#define CHANNELS 2
-#define BITRATE 320000
+const unsigned short FRAME_MILLIS = 20;
+const unsigned short FRAME_SIZE = 960;
+const unsigned short SAMPLE_RATE = 48000;
+const unsigned short CHANNELS = 2;
+const unsigned int BITRATE = 320000;
 
 #define MAX_PACKET_SIZE FRAME_SIZE * 8
 
@@ -29,28 +29,12 @@ using namespace utility;
 using namespace web::websockets::client;
 using namespace boost::asio::ip;
 
-/*
-void DiscordCPP::udp_client::wait_for_receive() {
-	//_socket->async_receive_from(boost::asio::buffer(_recv_buffer), 
-	//	_remote, 
-	//	boost::bind(&udp_client::handle_receive, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
-}
 
-void DiscordCPP::udp_client::handle_receive(boost::system::error_code &error, size_t bytes_tranferred) {
-	if (error) {
-		Logger("discord.VoiceClient.udp_client.handle_receive").error("Receive failed: " + error.message());
-	}
-	else {
-		Logger("discord.VoiceClient.udp_client.handle_receive").debug(string(_recv_buffer.begin(), _recv_buffer.begin() + bytes_tranferred));
-	}
-	wait_for_receive();
-}
-*/
 DiscordCPP::udp_client::udp_client() {
 
 }
 
-DiscordCPP::udp_client::udp_client(string_t ip, int port) {
+DiscordCPP::udp_client::udp_client(const string_t& ip, const int port) {
 	_log = Logger("discord.VoiceClient.udp_client");
 
 	udp::resolver resolver(_io_service);
@@ -69,23 +53,23 @@ DiscordCPP::udp_client::~udp_client() {
 	delete _socket;
 }
 
-void DiscordCPP::udp_client::send(const string &msg) {
+void DiscordCPP::udp_client::send(const string& msg) {
 	try {
 		_socket->send_to(boost::asio::buffer(msg, msg.size()), _remote);
 	}
-	catch (exception &e) {
+	catch (exception& e) {
 		_log.error("Cannot send message: " + string(e.what()));
 	}
 }
 
 string DiscordCPP::udp_client::receive() {
 	size_t len = 0;
-	
+
 	try {
 		udp::endpoint sender_enpoint;
 		len = _socket->receive_from(boost::asio::buffer(_recv_buffer), sender_enpoint);
 	}
-	catch (exception &e) {
+	catch (exception& e) {
 		_log.error("Cannot receive message: " + string(e.what()));
 	}
 
@@ -99,16 +83,24 @@ pplx::task<void> DiscordCPP::VoiceClient::connect_voice_udp() {
 		_log.info("performing IP Discovery");
 
 		string msg;
-		msg.resize(70, '\0');
-		for (int i = 0; i < 4; i++) {
-			msg[3 - i] = (_ssrc >> (i * 8));
-		}
+		msg.resize(74, '\0');
+		// Type: 0x1 for request, 0x2 for response
+		msg[0] = 0x1 >> 8;
+		msg[1] = 0x1;
+		// Message length excluding Type and Length fields (always 70)
+		msg[2] = 70 >> 8;
+		msg[3] = 70;
+		// SSRC
+		msg[4] = _ssrc >> 24;
+		msg[5] = _ssrc >> 16;
+		msg[6] = _ssrc >> 8;
+		msg[7] = _ssrc;
+
 		_udp->send(msg);
 
 		string recv_msg = _udp->receive();
-		//_log.debug("Received message(length: " + to_string(msg.size()) + "): " + msg);
 
-		string my_ip = recv_msg.substr(4, 16);
+		string my_ip = recv_msg.substr(8, 20);
 		for (unsigned int i = 0; i < my_ip.size(); i++) {
 			if (my_ip[i] == 0) {
 				my_ip.resize(i, '\0');
@@ -116,9 +108,7 @@ pplx::task<void> DiscordCPP::VoiceClient::connect_voice_udp() {
 			}
 		}
 
-		_log.debug("my_ip(str.length: " + to_string(my_ip.size()) + "): " + my_ip);
-
-		unsigned short my_port = (recv_msg[68] << 8) | (recv_msg[69]);
+		unsigned short my_port = (recv_msg[72] << 8) | (recv_msg[73]);
 		_log.debug("my_port: " + to_string(my_port));
 
 		my_port = (my_port >> 8) | (my_port << 8);
@@ -146,7 +136,7 @@ pplx::task<void> DiscordCPP::VoiceClient::select_protocol() {
 	});
 }
 
-pplx::task<void> DiscordCPP::VoiceClient::load_session_description(value data) {
+pplx::task<void> DiscordCPP::VoiceClient::load_session_description(const value& data) {
 	return pplx::create_task([this, data] {
 		_mode = data.at(U("mode")).as_string();
 		web::json::array tmp = data.at(U("secret_key")).as_array();
@@ -170,7 +160,7 @@ pplx::task<void> DiscordCPP::VoiceClient::speak(bool speak) {
 	});
 }
 
-DiscordCPP::VoiceClient::VoiceClient(MainGateway **main_ws, string_t voice_token, string_t endpoint, string_t session_id, string_t guild_id, string_t channel_id, string_t user_id) {
+DiscordCPP::VoiceClient::VoiceClient(MainGateway** main_ws, const string_t& voice_token, const string_t& endpoint, const string_t& session_id, const string_t& guild_id, const string_t& channel_id, const string_t& user_id) {
 	_guild_id = guild_id;
 	_channel_id = channel_id;
 
@@ -196,12 +186,6 @@ DiscordCPP::VoiceClient::VoiceClient(MainGateway **main_ws, string_t voice_token
 				_log.info("handshake complete. voice connection ready.");
 				_ready = true;
 				speak();
-				//pplx::create_task([this] {
-				//	while (_keepalive) {
-				//		string msg = _udp->receive();
-				//		_log.debug("Received message(length: " + to_string(msg.size()) + "): " + msg);
-				//	}
-				//});
 			});
 			break;
 		case 6:
@@ -215,7 +199,7 @@ DiscordCPP::VoiceClient::VoiceClient(MainGateway **main_ws, string_t voice_token
 
 	_voice_ws->connect(conversions::to_utf8string(endpoint));
 
-	while (_ready == false)	{
+	while (_ready == false) {
 		waitFor(chrono::milliseconds(10)).wait();
 	}
 }
@@ -250,11 +234,11 @@ pplx::task<void> DiscordCPP::VoiceClient::disconnect() {
 
 /**	@throws	OpusError	ClientException
 */
-pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
+pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource* source) {
 	return pplx::create_task([this, source] {
-		//_log.debug("creating opus encoder");
+		_log.debug("creating opus encoder");
 		int error;
-		OpusEncoder *encoder = opus_encoder_create(SAMPLE_RATE, CHANNELS, OPUS_APPLICATION_AUDIO, &error);
+		OpusEncoder* encoder = opus_encoder_create(SAMPLE_RATE, CHANNELS, OPUS_APPLICATION_AUDIO, &error);
 		if (error < 0) {
 			throw OpusError("failed to create opus encoder: " + string(opus_strerror(error)), error);
 		}
@@ -263,23 +247,23 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 		if (error < 0) {
 			throw OpusError("failed to set bitrate for opus encoder: " + string(opus_strerror(error)), error);
 		}
-		
+
 		opus_encoder_ctl(encoder, OPUS_SET_INBAND_FEC(1));
 		opus_encoder_ctl(encoder, OPUS_SET_PACKET_LOSS_PERC(15));
 
 
-		//_log.debug("initialising libsodium");
+		_log.debug("initialising libsodium");
 		if (sodium_init() == -1) {
 			throw ClientException("libsodium initialisation failed");
 		}
 
 		int num_opus_bytes;
-		unsigned char *pcm_data = new unsigned char[FRAME_SIZE * CHANNELS * 2];
-		opus_int16 *in_data;
+		unsigned char* pcm_data = new unsigned char[FRAME_SIZE * CHANNELS * 2];
+		opus_int16* in_data;
 		vector<unsigned char> opus_data(MAX_PACKET_SIZE);
-		
-		//_log.debug("starting loop");
-		
+
+		_log.debug("starting loop");
+
 		class timer_event {
 			bool is_set = false;
 
@@ -290,41 +274,29 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 			void unset() { is_set = false; };
 		};
 
-		timer_event *run_timer = new timer_event();
+		timer_event* run_timer = new timer_event();
 		run_timer->set();
 
-		queue<string> *buffer=new queue<string>();
-		
+		queue<string>* buffer = new queue<string>();
+
 		auto timer = pplx::create_task([run_timer, this, buffer] {
 			waitFor(chrono::milliseconds(5 * FRAME_MILLIS)).wait();
 			while (run_timer->get_is_set() || buffer->size() > 0) {
-#ifdef _WIN32
 				waitFor(chrono::milliseconds(FRAME_MILLIS)).then([this, buffer] {
 					if (buffer->size() > 0) {
 						_udp->send(buffer->front());
 						buffer->pop();
 					}
 				}).wait();
-#else
-				this_thread::sleep_for(chrono::milliseconds(FRAME_MILLIS));
-				if (buffer->size() > 0) {
-					_udp->send(buffer->front());
-					buffer->pop();
-			}
-#endif
 			}
 		});
 
-		while(1) {
+		while (1) {
 			if (buffer->size() >= 20) {
-#ifdef _WIN32
 				waitFor(chrono::milliseconds(FRAME_MILLIS)).wait();
-#else
-				this_thread::sleep_for(chrono::milliseconds(FRAME_MILLIS));
-#endif
 			}
 
-			if (source->read((char *)pcm_data, FRAME_SIZE * CHANNELS * 2) != true)
+			if (source->read((char*)pcm_data, FRAME_SIZE * CHANNELS * 2) != true)
 				break;
 
 			in_data = reinterpret_cast<opus_int16*>(pcm_data);
@@ -334,17 +306,15 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 				throw OpusError("failed to encode frame: " + string(opus_strerror(num_opus_bytes)), num_opus_bytes);
 			}
 
-			//_log.debug(to_string(num_opus_bytes) + "/" + to_string(MAX_PACKET_SIZE) + " used");
-
 			opus_data.resize(num_opus_bytes);
-			
+
 			vector<unsigned char> packet(12 + opus_data.size() + crypto_secretbox_MACBYTES);
 
 			packet[0] = 0x80;	//Type
 			packet[1] = 0x78;	//Version
 
 			packet[2] = _sequence >> 8;	//Sequence
-			packet[3] = (unsigned char) _sequence;
+			packet[3] = (unsigned char)_sequence;
 
 			packet[4] = _timestamp >> 24;	//Timestamp
 			packet[5] = _timestamp >> 16;
@@ -358,10 +328,10 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 
 			_sequence++;
 			_timestamp += SAMPLE_RATE / 1000 * FRAME_MILLIS;
-			
+
 			unsigned char nonce[crypto_secretbox_NONCEBYTES];
 			memset(nonce, 0, crypto_secretbox_NONCEBYTES);
-			
+
 			for (int i = 0; i < 12; i++) {
 				nonce[i] = packet[i];
 			}
@@ -369,14 +339,14 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 			crypto_secretbox_easy(packet.data() + 12, opus_data.data(), opus_data.size(), nonce, _secret_key.data());
 
 			packet.resize(12 + opus_data.size() + crypto_secretbox_MACBYTES);
-			
+
 			string msg;
 			msg.resize(packet.size(), '\0');
 
 			for (unsigned int i = 0; i < packet.size(); i++) {
 				msg[i] = packet[i];
 			}
-			
+
 			buffer->push(msg);
 		}
 
@@ -389,7 +359,7 @@ pplx::task<void> DiscordCPP::VoiceClient::play(AudioSource *source) {
 		opus_encoder_destroy(encoder);
 
 		delete[] pcm_data;
-		
-		//_log.debug("finished playing audio");
+
+		_log.debug("finished playing audio");
 	});
 }
